8a13190cf47744be36adbc97590dca0a8602a153
System Administrator <admin@example.com>
2019-10-29 10:54:34 -0400

    Changes by 'System Administrator' on 'DevOps Global - 1.0.0' -- Build #1

---
+++ sn/DevOps Global/Dictionary Entry/u_devops_global/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : DevOps Global
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-10-29 14:52:57
+ * Created By  : admin
+ * Updated On  : 2019-10-29 14:52:57
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=68780d25db700010343f58b3ca961959
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
